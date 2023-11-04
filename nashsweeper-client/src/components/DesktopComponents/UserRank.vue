4fe4f3d516ed6177a08cba6166ceffa0b77bb09a
Qian Zehao <qianzehao123@gmail.com>
2023-11-04 22:43:37 +0000

    update user rank

--- nashsweeper-client/src/components/DesktopComponents/UserRank.vue
+++ nashsweeper-client/src/components/DesktopComponents/UserRank.vue
@@ -6,19 +6,19 @@
         <table class="table-fixed w-full border-collapse">
             <thead>
                 <tr>
-                    <th class="w-1/4 border">Index</th>
-                    <th class="w-1/4  border">Player ID</th>
-                    <th class="w-1/4  border">Total strategies</th>
-                    <th class="w-1/4  border">Time(s)</th>
+                    <th class="w-1/4 border">PlayerID</th>
+                    <th class="w-1/4  border">BR find</th>
+                    <th class="w-1/4  border">Total Strategies</th>
+                    <th class="w-1/4  border">Time</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="Player in PlayerInfo" v-bind:key="Player">
                     <td class="border">
-                        <div class="flex justify-center items-center"> {{ Player.index }}</div>
+                        <div class="flex justify-center items-center"> {{ Player.PlayerID }}</div>
                     </td>
                     <td class="border">
-                        <div class="flex justify-center items-center"> {{ Player.PlayerID }}</div>
+                        <div class="flex justify-center items-center"> {{ Player.BRfind }}</div>
                     </td>
                     <td class="border">
                         <div class="flex justify-center items-center"> {{ Player.TotalStrategies }}</div>
@@ -45,8 +45,11 @@
 
 <script>
 import axios from 'axios';
-// if fail linking to the backend server, show the Mock data
 import { RatingData } from './PlayerRateMockData/RatingMockData.js'
+// import { KintoneRestAPIClient } from "@kintone/rest-api-client";
+// 
+// 
+
 export default {
     name: "UserRank",
     components: {},
@@ -61,28 +64,44 @@ export default {
             var that = this;
             axios({
                 method: 'get',
-                url: '/api/GetUserData'
+                // url: '/api/GetUserData',
+                url: '/nodebridge/get-records'
             })
                 .then(function (response) {
-                    // 处理成功情况
-                    console.log(response.data);
+                    // console.log(response.data);
                     // that.items = response.data;
-                    that.PlayerInfo = response.data;
+                    const originalData = response.data;
+                    // 
+                    const transformedData = originalData.map(item => ({
+                        BRfind: item.brfind.value,
+                        PlayerID: item.playerid.value,
+                        TotalStrategies: parseInt(item.totoalstrategies.value, 10),
+                        Time: item.time.value
+                    }));
+
+                    console.log(transformedData);
+
+
+                    that.PlayerInfo = transformedData;
                     // that.name = response.data.name;
                     // that.age = response.data.age;
                     // that.address = response.data.address;
-
+                    // 
                 })
                 .catch(function (error) {
                     // 处理错误情况
                     // console.log(error);
                     console.log(error.message);
-
+                    // 
                 })
         },
     },
     mounted() {
         this.getData();
+
+        // 
+        // 
+        // 
     },
 }
 </script>
