0f266ce61c93f00c437a17847fc0c082b87dc478
Qian Zehao <qianzehao123@gmail.com>
2023-11-04 22:43:11 +0000

    add proxy

--- nashsweeper-client/vite.config.js
+++ nashsweeper-client/vite.config.js
@@ -9,37 +9,19 @@ export default defineConfig({
   server: {
     watch: { usePolling: true },
     proxy: {
-      // // UploadPage CORS
-      // "/UploadPage": {
-      //   // target: "http://127.0.0.1:5000",
-      //   target: BackendServerInfo.backendIP,
-      //   changeOrigin: true,
-      //   pathRewrite: {
-      //     "^/UploadPage": "/UploadPage",
-      //   },
-      // },
-      // "/GetGameData": {
-      //   // target: "http://127.0.0.1:5000",
-      //   target: BackendServerInfo.backendIP,
-      //   changeOrigin: true,
-      //   pathRewrite: {
-      //     "^/GetGameData": "/GetGameData",
-      //   },
-      // },
-      // // GetUserData CORS
-      // "/GetUserData": {
-      //   // target: "http://127.0.0.1:5000",
-      //   target: BackendServerInfo.backendIP,
-      //   changeOrigin: true,
-      //   pathRewrite: {
-      //     "^/GetUserData": "/GetUserData",
-      //   },
-      // },
+      // 
       "/api": {
         target: BackendServerInfo.backendIP,
         changeOrigin: true,
         pathRewrite: {
           "^/api": "",
+        },
+      },
+      "/nodebridge": {
+        target: BackendServerInfo.nodebridgeIP,
+        changeOrigin: true,
+        pathRewrite: {
+          "^/nodebridge": "",
         },
       },
     },
